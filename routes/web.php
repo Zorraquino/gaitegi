7135b64a23f7e77710429bebadf5bd07a616fc3c
g.alava <g.alava@zorraquino.com>
2024-11-13 16:14:52 +0100

    Updates

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,17 @@ use Illuminate\Support\Facades\App;
 use App\Http\Controllers\EmailController;
 use App\Http\Controllers\PageController;
 
-Route::get('/{locale?}/{slug?}', [PageController::class, 'show'])
-    ->where(['locale' => 'eu|es'])
-    ->name('page.show');
+Route::middleware('cache.headers:public;max_age=2628000;etag')->prefix('build')->group(function () {
+    Route::get('/{any}', function ($any) {
+        return response()->file(public_path("build/{$any}"));
+    })->where('any', '.*');
+});
+
+// Existing routes
+Route::middleware('cache.headers:public;max_age=2628000;etag')->group(function () {
+    Route::get('/{locale?}/{slug?}', [PageController::class, 'show'])
+        ->where(['locale' => 'eu|es'])
+        ->name('page.show');
+});
 
 Route::post('/send-email', [EmailController::class, 'sendEmail'])->name('send.email');
