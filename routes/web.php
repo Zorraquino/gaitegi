836729607a514359d914f090496325fd575a1164
g.alava <g.alava@zorraquino.com>
2024-11-14 13:34:34 +0100

    Cleanup

--- routes/web.php
+++ routes/web.php
@@ -5,16 +5,8 @@ use Illuminate\Support\Facades\App;
 use App\Http\Controllers\EmailController;
 use App\Http\Controllers\PageController;
 
-// Route::middleware('cache.headers:public;max_age=2628000;etag')->group(function () {
-
-    Route::get('/{locale?}/{slug?}', [PageController::class, 'show'])
-        ->where(['locale' => 'eu|es'])
-        ->name('page.show');
-
-    // Route::get('/{any}', function ($any) {
-    //     return response()->file(public_path("build/{$any}"));
-    // })->where('any', '.*');
-
-// });
+Route::get('/{locale?}/{slug?}', [PageController::class, 'show'])
+    ->where(['locale' => 'eu|es'])
+    ->name('page.show');
 
 Route::post('/send-email', [EmailController::class, 'sendEmail'])->name('send.email');
